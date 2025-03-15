0528b3840c7d440e0aaf4c93f65151eeb715b6fa
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-03-15 22:37:28 +0900

    initial commit

---
+++ scripts/genx/scripts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NUM_PROCESSES=5  # set to the number of parallel processes to use
+DATA_DIR=/path/to/input
+DATASET=gen4 ## gen1 or gen4
+DT=(5 10 20 50 100)  # Different duration values
+
+for dt in "${DT[@]}"; do
+    DEST_DIR="/path/to/output/${DATASET}_preprocessed/dt_${dt}"  # Dynamic output directory
+    CONFIG_DURATION="conf_preprocess/extraction/duration_${dt}.yaml"  # Dynamic YAML file
+
+    echo "Processing with dt=${dt}, saving to ${DEST_DIR}, using config ${CONFIG_DURATION}"
+
+    python3 preprocess_dataset.py "${DATA_DIR}" "${DEST_DIR}" \
+        conf_preprocess/representation/stacked_hist.yaml \
+        "${CONFIG_DURATION}" \
+        conf_preprocess/filter_${DATASET}.yaml \
+        -ds ${DATASET} -np "${NUM_PROCESSES}"
+done
