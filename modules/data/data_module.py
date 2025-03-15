c48fa981e3c8a190c4bef672ee7be2cc349997ca
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-03-15 22:45:43 +0900

    modify

--- modules/data/data_module.py
+++ modules/data/data_module.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader, Dataset
 
 from data.genx_utils.collate import custom_collate_rnd, custom_collate_streaming
 from data.genx_utils.dataset_rnd import build_random_access_dataset, get_weighted_random_sampler, CustomConcatDataset
-from data.genx_utils.dataset_stream import build_streaming_dataset
+from data.genx_utils.dataset_streaming import build_streaming_dataset
 from data.utils.spatial import get_dataloading_hw
 from data.utils.types import DatasetMode, DatasetSamplingMode
 
