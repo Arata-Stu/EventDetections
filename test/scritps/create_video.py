e7d9879a3705c403e6d434abd6f65e38247a0a05
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-04-26 11:53:45 +0900

    update

--- test/scritps/create_video.py
+++ test/scritps/create_video.py
@@ -22,12 +22,15 @@ def main(cfg: DictConfig):
     fps = cfg.fps
     num_sequence = cfg.num_sequence
     dataset_mode = cfg.dataset_mode
+    ckpt_path = cfg.ckpt_path
 
     ## データセットの読み込み
     data = fetch_data_module(config=cfg)
     ## モデルの読み込み
-    model = fetch_model_module(config=cfg)
-
+    module = fetch_model_module(config=cfg)
+    module_class = type(module)
+    if ckpt_path is not None:
+        model = module_class.load_from_checkpoint(module, ckpt_path=ckpt_path)
     ##ビデオの作成
     create_video(data, model, show_gt, show_pred, output_path, fps, num_sequence, dataset_mode)
 
