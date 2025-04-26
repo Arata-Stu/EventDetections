ad2265e237914872739581db993ff012f5bbe066
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-04-26 12:47:38 +0900

    update

--- test/scritps/video.sh
+++ test/scritps/video.sh
@@ -1,13 +1,30 @@
+# ループさせたいDTの値
+DT_VALUES=(5 10 20 50 100)
+T_BIN=1
+CHANNEL=2
+
 MODEL=rvt ## rvt, rvt_ssm, yolox, yolox_lstm
 SIZE=tiny ## tiny small base
 DATASET=gen4 ## gen1, gen4, VGA (640*480 ev cam)
-EV_REPR_NAME='stacked_histogram_dt=5_nbins=1'
-INPUT_PATH=/media/arata-22/AT_SSD/dataset/gen4_preprocessed_bins_1/dt_5 # preprocessed dataset
-OUTPUT_VIDEO=dt_5.mp4
-CKPT_PATH=/home/arata-22/Downloads/EventDetections/rvt_gen4_bins_1/dt_5_best.ckpt
-BINS=1
-CHANNEL=2
-python3 create_video.py \
-model=${MODEL} +model/${MODEL}=${SIZE}.yaml model.backbone.input_channels=${CHANNEL} \
-dataset=${DATASET} dataset.path=${INPUT_PATH} dataset.ev_repr_name="'${EV_REPR_NAME}'" \
-output_path=${OUTPUT_VIDEO} gt=False pred=False num_sequence=1 ckpt_path=${CKPT_PATH} \
+
+GT=False # add gt to video
+PREDICTION=False # add prediction to video
+num_sequence=1
+USE_BEST=True # True: best.ckpt, False: last.ckpt
+
+for DT in "${DT_VALUES[@]}"; do
+    DATA_DIR="/home/aten-22/dataset/${DATASET}_preprocessed_bins_${T_BIN}/dt_${DT}"
+    OUTPUT_VIDEO="${DATASET}_bin_${T_BIN}_dt_${DT}.mp4"
+
+    if [ "$USE_BEST" = True ]; then
+        CKPT_NAME="best.ckpt"
+    else
+        CKPT_NAME="last.ckpt"
+    fi
+    CKPT_PATH="./ckpts/${MODEL}_${DATASET}_bins_${T_BIN}/dt_${DT}_${CKPT_NAME}"
+
+    python3 create_video.py \
+        model=${MODEL} +model/${MODEL}=${SIZE}.yaml model.backbone.input_channels=${CHANNEL} \
+        dataset=${DATASET} dataset.path=${DATA_DIR} dataset.ev_repr_name="'stacked_histogram_dt=${DT}_nbins=${T_BIN}'" \
+        output_path=${OUTPUT_VIDEO} gt=${GT} pred=${PREDICTION} num_sequence=${num_sequence} ckpt_path=${CKPT_PATH}
+done
